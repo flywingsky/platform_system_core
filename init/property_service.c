996e35daacf55c499d2d74d02e2a2f4cd9a325c9
Julien Bolard <jbolard@genymobile.com>
2013-11-19 00:13:09 +0100

    [BZ256] Allow genyd. properties modification from system uid daemons to fix dropped androVM_setprop setuid flag,
    system server was not able to cache battery properties, and not able to switch to manual mode by default for
    desktop computers too. This might come from the 'no setuid' security enhancement of the 4.3 version.

--- init/property_service.c
+++ init/property_service.c
@@ -93,7 +93,8 @@ struct {
     { "persist.service.", AID_SYSTEM,   0 },
     { "persist.security.", AID_SYSTEM,   0 },
     { "persist.service.bdroid.", AID_BLUETOOTH,   0 },
-    { "selinux."         , AID_SYSTEM,   0 },
+    { "selinux."        , AID_SYSTEM,   0 },
+    { "genyd."          , AID_SYSTEM,   0 },
     { NULL, 0, 0 }
 };
 
