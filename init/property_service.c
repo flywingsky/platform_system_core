ee509a02135d624b27b8ac142184b60650bbd5b7
Julien Bolard <jbolard@genymobile.com>
2013-12-06 10:40:33 +0100

    [BZ256] Allow genyd. properties modification from system uid daemons to fix dropped androVM_setprop setuid flag.

--- init/property_service.c
+++ init/property_service.c
@@ -92,8 +92,7 @@ struct {
     { "persist.sys.",     AID_SYSTEM,   0 },
     { "persist.service.", AID_SYSTEM,   0 },
     { "persist.security.", AID_SYSTEM,   0 },
-    { "persist.service.bdroid.", AID_BLUETOOTH,   0 },
-    { "selinux."         , AID_SYSTEM,   0 },
+    { "genyd.",           AID_SYSTEM,   0 },
     { NULL, 0, 0 }
 };
 
